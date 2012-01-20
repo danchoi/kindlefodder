74ca742168cecbdb6c7955328454f4e251213321
Daniel Choi <dhchoi@gmail.com>
2012-01-20 09:24:00 -0500

    fixups

--- lib/docs_on_kindle.rb
+++ lib/docs_on_kindle.rb
@@ -17,6 +17,37 @@ module DocsOnKindle
 
   STYLESHEET = File.absolute_path "css/kindle.css"
 
+  def build_kindlerb_tree
+    sections = YAML::load_file "#{OUTPUT_DIR}/sections.yml"
+    sections.select! {|s| !s[:articles].empty?}
+    Dir.chdir OUTPUT_DIR do
+      sections.each_with_index {|s, section_idx|
+        title = s[:title]
+        FileUtils::mkdir_p("sections/%03d" % section_idx)
+        File.open("sections/%03d/_section.txt" % section_idx, 'w') {|f| f.puts title}
+        puts "sections/%03d -> #{title}" % section_idx
+        # save articles
+        s[:articles].each_with_index {|a, item_idx|
+          article_title = a[:title]
+          /(?<path>articles\/[\w-]+)(#\w+|)$/ =~ a[:url]
+          puts a[:url], path
+          item = Nokogiri::HTML(File.read path)
+
+          download_images! item
+          fixup_html! item
+
+          item_path = "sections/%03d/%03d.html" % [section_idx, item_idx] 
+          add_head_section item, article_title
+
+          File.open(item_path, 'w'){|f| f.puts item.to_html}
+          puts "  #{item_path} -> #{article_title}"
+        }
+      }
+      mobi!
+    end
+  end
+
+
   def add_head_section(doc, title)
     head = Nokogiri::XML::Node.new "head", doc
     title_node = Nokogiri::XML::Node.new "title", doc
@@ -30,7 +61,7 @@ module DocsOnKindle
     doc.at("body").before head
   end
 
-  def run cmd
+  def run_shell_command cmd
     puts "  #{cmd}"
     `#{cmd}`
   end
@@ -42,11 +73,11 @@ module DocsOnKindle
       FileUtils::mkdir_p 'images'
       FileUtils::mkdir_p 'grayscale_images'
       unless File.size?("images/#{img_file}")
-        run "curl -Ls '#{src}' > images/#{img_file}"
+        run_shell_command "curl -Ls '#{src}' > images/#{img_file}"
       end
       grayscale_image_path = "grayscale_images/#{img_file.gsub(/(\.\w+)$/, "-grayscale.gif")}"
       unless File.size?(grayscale_image_path)
-        run "convert images/#{img_file}[0] -type Grayscale -depth 8 -resize '400x300>' #{grayscale_image_path}"
+        run_shell_command "convert images/#{img_file}[0] -type Grayscale -depth 8 -resize '400x300>' #{grayscale_image_path}"
       end
       img['src'] = [Dir.pwd, grayscale_image_path].join("/")
     }
@@ -63,13 +94,14 @@ module DocsOnKindle
     # We want to remove nested 'p' tags in 'li' tags, because these introduce an undesirable 
     # blank line after the bullet. The expected CSS fix doesn't work.
     doc.search('li').each {|li|
-      xs = li.search("p").map {|p|
+      li.search("p").each {|p|
         # remove surrounding paragraph tags
         p.children.each {|c|
           li.add_child c
         }
         p.remove
-      }.flatten
+      }
+      # remove any spaces 
 
     }
 
