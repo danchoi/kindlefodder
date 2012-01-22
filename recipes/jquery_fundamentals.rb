41d22b8f14efed3ab667a4bb4e24f42e1f37b10f
Daniel Choi <dhchoi@gmail.com>
2012-01-22 09:48:13 -0500

    stub jquery fundamentals recipe

---
+++ recipes/jquery_fundamentals.rb
@@ -0,0 +1,66 @@
+require 'docs_on_kindle'
+
+class JqueryFundamentals < DocsOnKindle
+  
+
+  def get_source_files
+    sections = extract_sections
+    File.open("#{output_dir}/sections.yml", 'w') {|f|
+      f.puts sections.to_yaml
+    }
+  end
+
+  def document 
+    {
+      'title' => 'jQuery Fundamentals',
+      'author' => 'Rebecca Murphey',
+      'cover' => nil,
+      'masthead' => nil
+    }
+  end
+
+  def extract_sections
+    url = 'http://jqfundamentals.com/'
+    html = `curl -Ls #{url}`
+    doc = Nokogiri::HTML  html
+    [frontmatter(doc)] + doc.search('.chapter').map {|chapter|
+      chapter_title = chapter['title']
+      {
+        title: chapter_title,
+        articles: chapter.search('.section').map {|section|
+          title = section['title']
+          content = section.inner_html
+          path = "#{chapter_title}.#{title}".downcase.gsub(/\W/, '_')
+          File.open("#{output_dir}/#{path}", 'w') {|f| f.puts content}
+
+          {
+            title: section['title'],
+            path: path
+          }
+
+        }
+      }
+    }
+  end
+
+  def frontmatter doc
+    x = doc.at('#titlepage')
+    content = x.inner_html
+    path = "frontmatter.frontmatter"
+    File.open("#{output_dir}/#{path}", 'w') {|f| f.puts content}
+    article = { 
+      title: x.at("h1.title").inner_text.strip,
+      path: path
+    }
+
+    {
+      title: "Frontmatter",
+      articles: [ article ]
+    }
+  end
+
+ 
+end
+
+JqueryFundamentals.generate
+
